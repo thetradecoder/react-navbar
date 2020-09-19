8d4824b8d0f2e9dc4c8cc2d92f69ca9cad03f4bb
Mamun Abdullah <21096193+thetradecoder@users.noreply.github.com>
2020-09-19 15:42:20 +0600

    Update navbar.component.js

--- navbar.component.js
+++ navbar.component.js
@@ -1,1 +1,27 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import 'jquery/dist/jquery';
 
+export default function Navbar(){
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <div className="navbar-header">
+                    <a href="/" className="navbar-brand">tradecoder</a>
+                </div>
+                    <button data-target="#main-menu" data-toggle="collapse" className="navbar-toggler">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div id="main-menu" className="collapse navbar-collapse">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item"><Link to="/link-one/" className="nav-link">Link One</Link></li>
+                            <li className="nav-item"><Link to="/link-two/" className="nav-link">Link Two</Link></li>
+                            <li className="nav-item"><Link to="/link-three/" className="nav-link">Link Three</Link></li>
+                        </ul>
+                    </div>       
+            </div>
+        </nav>
+    );
+}
